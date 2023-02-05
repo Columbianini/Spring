435163f6cd729d2dfac4f1d83de5ab0a366afcc1
Tony An <13816554795@163.com>
2023-02-05 10:30:53 +0800

    inject database repo to business logic

--- tacocloud/src/main/java/sia/tacocloud/OrderController.java
+++ tacocloud/src/main/java/sia/tacocloud/OrderController.java
@@ -1,6 +1,7 @@
 package sia.tacocloud;
 
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,13 @@ import sia.tacocloud.TacoOrder;
 @RequestMapping("/orders")
 @SessionAttributes("tacoOrder")
 public class OrderController {
+    private final OrderRepository orderRepository;
+
+    @Autowired
+    public OrderController(OrderRepository orderRepository){
+        this.orderRepository = orderRepository;
+    }
+
     @GetMapping("/current")
     public String orderForm(){
         return "orderForm";
@@ -27,7 +35,7 @@ public class OrderController {
             return "orderForm";
         }
 
-        log.info("Order submitted: {}", order);
+        orderRepository.save(order);
         sessionStatus.setComplete();
         return "redirect:/";
     }
